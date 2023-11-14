eea546a2fef6b661bd0c6090f9388e5e0ee36484
LakshmiKMVinayaBhakshi <lakshmikm.h9@outlook.com>
2023-11-14 18:58:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, {useState, useEffect}from 'react';
+import './App.css';
+
+function App() {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+
+    if (isRunning) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(interval);
+  }, [isRunning]);
+
+  const handleStartStop = () => {
+    setIsRunning((prevIsRunning) => !prevIsRunning);
+  };
+
+  const handleReset = () => {
+    setTime(0);
+    setIsRunning(false);
+  };
+
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+  };
+
+  return (
+    <div className="App">
+      <h1>React Stopwatch</h1>
+      <div className="stopwatch">{formatTime(time)}</div>
+      <div className="controls">
+        <button onClick={handleStartStop}>{isRunning ? 'Stop' : 'Start'}</button>
+        <button onClick={handleReset}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
